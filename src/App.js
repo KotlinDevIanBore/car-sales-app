9f7d7b0066e28767f027e3c5e88a222fbcb4a104
KotlinDevIanBore <iankipchirchirb3@gmail.com>
2024-04-05 14:54:57 +0300

    Added more styles

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-<div className='grid'>
+    <div> 
+      <nav className="top-navigation"> navigation</nav>
+      
+      <div className="grid">
+      <div className="car-container div-1">
+        <img
+          className="car-image"
+          src="/car-pictures/discovery-front.jpg"
+          alt=""
+        />
 
-  <div className='car-container div-1'> 
-  <img src="discovery-front.jpg"/> 
-  </div>
+        <div className="car-name">LandRover Discovery</div>
+        <div className="car-price">5000000</div>
+      </div>
 
+      <div className="car-container div-2"> div 2 </div>
+      <div className="car-container div-1"> div 3 </div>
+      <div className="car-container div-2"> div 4 </div>
+      <div className="car-container div-2"> div 5 </div>
+      <div className="car-container div-1"> div 6 </div>
+      <div className="car-container div-2"> div 7 </div>
+      <div className="car-container div-1"> div 8 </div>
+    </div>
 
-  <div className='car-container div-2'> div 2 </div>
-  <div className='car-container div-1'> div 3 </div>
-  <div className='car-container div-2'> div 4 </div>
-  <div className='car-container div-2'> div 5 </div>
-  <div className='car-container div-1'> div 6 </div>
-
-
-
-
-
-
-
-
-
-</div>
+    </div>
+    
   );
 }
 
