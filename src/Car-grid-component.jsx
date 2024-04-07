be95712eae53d6103401e29ba6143b521d726c8a
KotlinDevIanBore <iankipchirchirb3@gmail.com>
2024-04-07 13:15:01 +0300

    Added code for changing the images

--- src/Car-grid-component.jsx
+++ src/Car-grid-component.jsx
@@ -1,22 +1,47 @@
-/* eslint-disable no-undef */
 import "./App.css";
+import { useState } from "react";
 
 function CarGrid(props) {
-    console.log(`Here ${props.cars}`);
+
+    const [carArray,SetCar]= useState (props.cars)
+
+    function changeImageIndex(buttonId) {
+        console.log(`Button id: ${buttonId}`);
+
+       const cars= props.cars.map ((car)=>{
+
+        if (car.id===buttonId){
+
+            return {... car, image: 'require("./car-pictures/discovery3.jpg")'}
+
+        }
+
+        else{
+            return car;
+        }
+
+        })
+        SetCar(cars)
+
+    }
+
     return (
         <div>
             <nav className="top-navigation"> Navigation Bar</nav>
             <div className="grid">
-                {props.cars.map((car) => (
-                    <div className="car-container div-2" key={car.name}>
+                {carArray.map((car) => (
+                    <div data-card-id={car.id} className="car-container div-2" key={car.name}>
                         <img
                             className="car-image"
-                            src={car.image[7].URL} // Accessing URL property
+                            src={car.image[0].URL} // Accessing URL property
                             alt=""
                         />
                         <div className="car-name">{car.name}</div>
                         <div className="car-price">{car.price}</div>
-                        <button className="add-to-cart">Add to Cart</button>
+                        <button
+                        onClick={() => { changeImageIndex(car.id) }}
+
+                        data-button-id={car.id} className="add-to-cart">Add to Cart</button>
                     </div>
                 ))}
             </div>
