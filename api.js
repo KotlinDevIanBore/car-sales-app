f2ec14f005a588164be7839b33e15a61a639ef20
KotlinDevIanBore <iankipchirchirb3@gmail.com>
2024-08-03 14:20:01 +0300

    (chore)Modified api url to match ne render deployment

--- api.js
+++ api.js
@@ -1,1 +1,5 @@
-export const API_URL = 'https://car-sales-app-server.onrender.com';
+// export const API_URL = 'https://car-sales-app-server.onrender.com';
+// export const API_URL = 'http://localhost:3000';
+export const API_URL = 'https://car-sales-app-server-1.onrender.com';
+
+
