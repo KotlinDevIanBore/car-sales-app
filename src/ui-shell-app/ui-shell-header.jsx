d594983bc0052df7d84c16561d7e7de27342f101
KotlinDevIanBore <iankipchirchirb3@gmail.com>
2024-08-05 12:02:09 +0300

    (chore)Made the stories ring component responsive

--- src/ui-shell-app/ui-shell-header.jsx
+++ src/ui-shell-app/ui-shell-header.jsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import CircularRing from '../stories-app/ring';
-import './ui-shell-header.scss'; // Import the SCSS file
-import { useInterval } from 'react-use';
-import { useState, useContext} from "react";
-import { searchContext } from '../home-page/home_page_context';
-
-
+import React from "react";
+import CircularRing from "../stories-app/ring";
+import "./ui-shell-header.scss"; // Import the SCSS file
+import { useInterval } from "react-use";
+import { useState, useContext } from "react";
+import { searchContext } from "../home-page/home_page_context";
 
 const UIShellHeader = () => {
-    const [intervalRunning, setIntervalRunning] = useState(false);
+  const [intervalRunning, setIntervalRunning] = useState(false);
 
-    const {handleRingClick}= useContext(searchContext)
+  const { handleRingClick } = useContext(searchContext);
 
-    const max_interval=10000;
+  const max_interval = 10000;
 
-  
-
-  
-
-
-  function changeStoryIndex(){
-
+  function changeStoryIndex() {
     setstoryIndex((prevIndex) => {
       if (prevIndex < 5) {
         return prevIndex + 1;
@@ -28,22 +20,23 @@ const UIShellHeader = () => {
         return 0;
       }
     });
-  
   }
 
   // setInterval (()=>{changeStoryIndex()},30000);
-  useInterval (changeStoryIndex, intervalRunning?max_interval:null)
-    return (
-        <div className="ui-shell-header">
-            {/* Your header content here */}
-            <div>Dream Cars Real Prices </div>
+  useInterval(changeStoryIndex, intervalRunning ? max_interval : null);
+  return (
+    <div className="ui-shell-header">
+     
 
-            <div className="stories-ring" onClick={handleRingClick} >
-      < CircularRing />
-
-      </div>
+      <div className="wrapper-element">
+        <div className="stories-ring" onClick={handleRingClick}>
+          <CircularRing />
         </div>
-    );
+        
+      </div>
+      <div>Dream Cars Real Prices </div>
+    </div>
+  );
 };
 
 export default UIShellHeader;
