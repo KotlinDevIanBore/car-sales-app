6f25b4f5b47cdbceb08ab81fa64654c73af66909
KotlinDevIanBore <iankipchirchirb3@gmail.com>
2024-08-05 23:15:06 +0300

    Removed emotion react

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
+// vite.config.js
+import { defineConfig } from 'vite';
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@emotion/react': 'node_modules/@emotion/react',
+    },
+  },
 });
-
