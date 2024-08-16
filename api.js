bb2a04f5b0974bec3e3932cf35d76c6e9d9f5858
KotlinDevIanBore <iankipchirchirb3@gmail.com>
2024-08-16 15:14:12 +0300

    Created the frontend and backend api for the image search

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
-// export const API_URL = 'https://car-sales-app-server-1.onrender.com';
 
-export const API_URL = 'http://localhost:3000';
+export const API_URL = 'https://car-sales-app-server-1.onrender.com';
+
+// export const API_URL = 'http://localhost:3000';
 
 
